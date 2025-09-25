86804e5bc2d68684e1d536ac4073b0202e541083
Traiwy <lavyou141@gmail.com>
2025-09-25 23:55:38 +0300

    Adding method work with Itemmeta

---
+++ src/main/java/traiwy/libTraiwy/item/ItemMeta.java
@@ -0,0 +1,16 @@
+package traiwy.libTraiwy.item;
+
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+
+public class ItemMeta {
+    public void meta(ItemStack item, String name, List<String> lore){
+        org.bukkit.inventory.meta.ItemMeta metaItem = item.getItemMeta();
+        if(metaItem != null){
+            metaItem.setDisplayName(name);
+            metaItem.setLore(lore);
+            item.setItemMeta(metaItem);
+        }
+    }
+}
